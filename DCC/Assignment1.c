b2875b879e86471bddaaf8830368de0f3ce1e62f
vedkulk <kulkarnived3@gmail.com>
2025-01-15 10:55:20 +0530

    ML assignment1, NLP assignment 2, DCC assignment1 started

---
+++ DCC/Assignment1.c
@@ -0,0 +1,11 @@
+#include<sys/socket.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#define PORT 9090
+
+int main(int argc, char const*argv[]){
+    
+}
